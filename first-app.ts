040c982d7010c340fc21dde9bce2a8df37c225ad
tmsby <kafa.thud.zaharo@gmail.com>
2024-11-04 09:51:20 +0900

    Type guards

--- first-app.ts
+++ first-app.ts
@@ -94,11 +94,18 @@ admin = {
   userName: "Max",
 };
 
-let role: "admin" | "user" | "editor";
+type Role = "admin" | "user" | "editor";
+let role: Role;
 
 role = "admin";
 role = "user";
 role = "editor";
 // role = "abc";
+
+function performAction(action: string | number, role: Role) {
+  if (role === "admin" && typeof action === "string") {
+    // ...
+  }
+}
 
 export {};
